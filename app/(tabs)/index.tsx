78639f105f4d22775bd1b0101a09af1d4ba52197
eyualpha <eyuashenafi533@gmail.com>
2026-01-30 22:09:31 +0300

    Add constants for icons and images, define movie interfaces, and declare PNG module

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,7 +3,7 @@ import { Text, View } from "react-native";
 export default function Index() {
   return (
     <View className=" flex-1 items-center justify-center">
-      <Text className="text-red-500">
+      <Text className="text-dark-200">
         Edit app/index.tsx to edit this screen.
       </Text>
     </View>
